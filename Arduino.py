7e6dc89753b87284921786a926ad3333690ce4b2
Andersson bustos padilla <anderssonsena95@gmail.com>
2026-07-07 09:24:59 -0500

    Refactor serial communication setup and add mypy configuration for type checking

--- Arduino.py
+++ Arduino.py
@@ -1,12 +1,11 @@
 import serial
 import time
 
-serialArduino = serial.Serial('COM3', 9600)
+serialArduino = serial.Serial("COM3", 9600)
 time.sleep(2)  # Espera a que Arduino inicialice
 
 
 while True:
-
     # Enviar '1' para encender
     serialArduino.write(bytes([1]))
     time.sleep(1)
