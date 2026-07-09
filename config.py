75c55a92d2756c380bb4b89bc4b770a4ee785f83
Andersson bustos padilla <anderssonsena95@gmail.com>
2026-07-08 23:02:19 -0500

    creacion de archivo de config con constantes de configuracion.

---
+++ config.py
@@ -0,0 +1,8 @@
+PUERTOARDUINO = 'COM3'  # Cambia esto al puerto correcto de tu Arduino
+BAUDIOS = 9600  # Cambia esto a la velocidad correcta de tu Arduino
+MAX_EMBEDDINGS = 30  # Número máximo de embeddings a almacenar
+SKIP_FRAMES = 1  # Número de frames a saltar antes de procesar uno
+UMBRAL_SIMILITUD = 0.8  # Umbral de similitud para considerar un rostro como conocido
+P_KALMAN=100.0
+R_KALMAN=2.0
+Q_KALMAN=0.1
